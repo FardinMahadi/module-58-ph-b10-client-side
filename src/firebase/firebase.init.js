5b023b53587ecad032aa0077eb5f7ec651f0dc3e
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-09 09:16:15 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: VITE_apiKey,
+  authDomain: VITE_authDomain,
+  projectId: VITE_projectId,
+  storageBucket: VITE_storageBucket,
+  messagingSenderId: VITE_messagingSenderId,
+  appId: VITE_appId"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
